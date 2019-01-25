7fb8c2ac0a75ae8743f9a4ced4d2df1a46f3e792
unipota <unipota@monolith-2.local>
2019-01-26 00:27:35 +0900

    split webpack conf

---
+++ webpack.dev.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const PrettierPlugin = require('prettier-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: ['./src/js/game.js'],
+  module: {
+    rules: [
+      {
+        type: 'javascript/auto',
+        test: /\.json$/,
+        use: [ { loader: 'file-loader' } ]
+      },
+      {
+        test: /\.html$/
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/public'
+  },
+  devServer: {
+    contentBase: __dirname + '/public',
+    watchContentBase: true,
+    port: 1234
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new CopyWebpackPlugin([
+      { from: 'src/assets/img', to: 'assets/img' }
+    ]),
+    new PrettierPlugin({
+      printWidth: 80,         
+      tabWidth: 2,
+      useTabs: false,
+      semi: false,
+      singleQuote: true,
+      trailingComma: 'none',
+      bracketSpacing: true,
+      encoding: 'utf-8',
+      extensions: [ '.js', '.ts' ]
+    })
+  ]
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const PrettierPlugin = require('prettier-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
+  mode: 'production',
   entry: ['./src/js/game.js'],
   module: {
     rules: [
@@ -21,28 +21,12 @@ module.exports = {
     filename: 'bundle.js',
     path: __dirname + '/public'
   },
-  devServer: {
-    contentBase: __dirname + '/public',
-    watchContentBase: true,
-    port: 1234
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
     }),
     new CopyWebpackPlugin([
       { from: 'src/assets/img', to: 'assets/img' }
-    ]),
-    new PrettierPlugin({
-      printWidth: 80,               
-      tabWidth: 2,                 
-      useTabs: false,              
-      semi: false,                  
-      singleQuote: true,
-      trailingComma: false,
-      bracketSpacing: true,
-      encoding: 'utf-8',           
-      extensions: [ ".js", ".ts" ]  
-    })
+    ])
   ]
 };
